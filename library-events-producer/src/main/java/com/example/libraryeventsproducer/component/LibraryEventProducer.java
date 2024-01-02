71acb8de997aace4390a466412ab6e89ac4f3611
Choi Tae gyu <suker800@gmail.com>
2024-01-02 21:16:42 +0900

    - 카프카 Library Producer 생성

--- library-events-producer/src/main/java/com/example/libraryeventsproducer/component/LibraryEventProducer.java
+++ library-events-producer/src/main/java/com/example/libraryeventsproducer/component/LibraryEventProducer.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.SendResult;
@@ -34,19 +35,44 @@ public class LibraryEventProducer {
 
         return completableFuture.whenComplete((sendResult, throwable) -> {
             if (throwable != null) {
-                heandleFailure(key, value, throwable);
+                handleFailure(key, value, throwable);
             } else {
-                heandleSuccess(key, value, sendResult);
+                handleSuccess(key, value, sendResult);
             }
         });
     }
 
-    private void heandleSuccess(Integer key, String value, SendResult<Integer, String> sendResult) {
+    public CompletableFuture<SendResult<Integer, String>> sendLibraryEventWithProducerRecord(LibraryEvent libraryEvent) throws JsonProcessingException {
 
-        log.info("Message sent successfully for the key : {} and value : {} partition is : {} ", key, value, sendResult, sendResult.getRecordMetadata().partition());
+        var key = libraryEvent.libraryEventId();
+        String value = objectMapper.writeValueAsString(libraryEvent);
+        ProducerRecord<Integer, String> producerRecord = buildProducerRecord(key, value);
+
+//        CompletableFuture<SendResult<Integer, String>> completableFuture = kafkaTemplate.send(topic, key, value);
+
+        CompletableFuture<SendResult<Integer, String>> completableFuture = kafkaTemplate.send(producerRecord);
+
+        return completableFuture.whenComplete((sendResult, throwable) -> {
+            if (throwable != null) {
+                handleFailure(key, value, throwable);
+            } else {
+                handleSuccess(key, value, sendResult);
+            }
+        });
     }
 
-    private void heandleFailure(Integer key, String value, Throwable throwable) {
+    private ProducerRecord<Integer, String> buildProducerRecord(Integer key, String value) {
+
+        return new ProducerRecord<>(topic, key, value);
+    }
+
+
+    private void handleSuccess(Integer key, String value, SendResult<Integer, String> sendResult) {
+
+        log.info("Message sent successfully for the key : {} and value : {} partition is : {} ", key, value, sendResult.getRecordMetadata().partition());
+    }
+
+    private void handleFailure(Integer key, String value, Throwable throwable) {
         log.error("Error sending the message and exception is {}", throwable.getMessage());
     }
 
--- library-events-producer/src/main/java/com/example/libraryeventsproducer/controller/LibraryEventsController.java
+++ library-events-producer/src/main/java/com/example/libraryeventsproducer/controller/LibraryEventsController.java
@@ -1,22 +1,30 @@
 package com.example.libraryeventsproducer.controller;
 
+import com.example.libraryeventsproducer.component.LibraryEventProducer;
 import com.example.libraryeventsproducer.domain.LibraryEvent;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
+@RequiredArgsConstructor
 public class LibraryEventsController {
+    private final LibraryEventProducer producer;
 
 
     @PostMapping("/v1/libraryevent")
-    public ResponseEntity<LibraryEvent> libraryEvent(@RequestBody LibraryEvent libraryEvent) {
+    public ResponseEntity<LibraryEvent> libraryEvent(@RequestBody LibraryEvent libraryEvent) throws JsonProcessingException {
 
         log.info("library event : {} ", libraryEvent);
 
+//        producer.sendLibraryEvent(libraryEvent);
+        producer.sendLibraryEventWithProducerRecord(libraryEvent);
         return ResponseEntity.ok(libraryEvent);
     }
 }
